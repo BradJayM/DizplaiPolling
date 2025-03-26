8b0f61f46aae1732f98e52810209b021a27299a9
BradJayM <bradjaymill@gmail.com>
2025-03-26 12:32:20 +0000

    Adds the poll and votes resources to app.js

--- Server/app.js
+++ Server/app.js
@@ -2,10 +2,24 @@ const express = require('express'); //Imports Express
 const app = express();  //Initialised instance of express applciation
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+//Store the polls using in-memory storage
+let polls = [
+    {
+        pollId:1,
+        pollName: "Best Coding Language",
+        question: "What is the best coding language?",
+        options: [
+            {optionId: 1, optionText: "Java"},
+            {optionId: 2, optionText: "C++"},
+            {optionId: 3, optionText: "MIPS"},
+        ],
+    },
+];
+
+//Store the polls using in-memory storage
+let votes = {}
 
 app.listen(port, () => {
-    console.log('Example app listening on port ${port}')
-})
+    console.log('Example app listening on port ${port}');
+});
+
